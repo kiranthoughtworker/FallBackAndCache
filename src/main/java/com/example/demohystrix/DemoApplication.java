f4b13adb643b2a9c49800fbde5c1f5b428146084
kiranvajrapu <kiran.vajrapu@thoughtwork.com>
2023-09-05 19:49:45 +0530

    Modified Hytrix Demo

--- src/main/java/com/example/demohystrix/DemoApplication.java
+++ src/main/java/com/example/demohystrix/DemoApplication.java
@@ -3,7 +3,6 @@ package com.example.demohystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-
 @EnableHystrix
 @SpringBootApplication
 public class DemoApplication {
